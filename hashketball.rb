2c9f54799c87f66f9935b5d030b0f52718eeabe4
Hunter Sides <hunter.s.1337@gmail.com>
2019-12-06 18:25:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -202,8 +202,17 @@ end
  
  #################################################################################
  
- def big_shoe_rebounds
- 
+ def rebounds_for_largest_shoe_size(game)
+  max_player = nil
+  game.each do |team, team_hash|
+    team_hash[:players].each do |player, player_hash|
+      max_player ||= player_hash
+      max_player = player_hash if player_hash[:shoe_size] > max_player[:shoe_size]
+    end
+  end
+
+  max_player[:stats][:rebounds]
+end
    
    
    
